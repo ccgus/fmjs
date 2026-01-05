9938d6012bb80792013bc6d8aa9764dc4f2734ef
August Mueller <gus@flyingmeat.com>
2026-01-05 09:43:54 -0800

    Change IBOutlet properties to weak references

--- FJSTestApp/FJSConsoleEntryViewController.h
+++ FJSTestApp/FJSConsoleEntryViewController.h
@@ -23,9 +23,9 @@ typedef NS_ENUM(NSUInteger, FJSConsoleEntryType) {
 @property (assign, nonatomic) FJSConsoleEntryType messageType;
 @property (strong) NSString *messageString;
 
-@property (assign) IBOutlet NSTextField *messageField;
-@property (assign) IBOutlet NSTextField *ioIndicator;
-@property (assign) IBOutlet NSBox *topLine;
+@property (weak) IBOutlet NSTextField *messageField;
+@property (weak) IBOutlet NSTextField *ioIndicator;
+@property (weak) IBOutlet NSBox *topLine;
 
 - (CGFloat)calculatedHeight;
 
